690df550b340219a79ad202508ff37063a348349
Reyinbee <wealthbassey99@gmail.com>
2024-12-31 19:10:23 +0100

    main

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-        <h1>DataAnalyst.me</h1>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/projects">Projects</a></li>
-          <li><a href="/skills">Skills</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        <div className="hamburger">
-          <button>☰</button>
-        </div>
-      </nav>
-      
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
-  
-  export default Navbar;
-  
+
+  return (
+    <nav className="navbar">
+      <h1>DataAnalyst.me</h1>
+      <ul className={isOpen ? 'open' : ''}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/projects">Projects</a></li>
+        <li><a href="/skills">Skills</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <button>☰</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
