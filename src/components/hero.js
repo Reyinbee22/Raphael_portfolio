97df9630586429508fd3f3ac0c59338d6b4a7071
Reyinbee <wealthbassey99@gmail.com>
2025-01-01 12:50:51 +0100

    main

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="hero-content">
         <h1>Hi, 
           <br/>
-          I'm Rapheal Oyelami</h1>
+          I'm Raphael Oyelami</h1>
         <p>Turning Data into Insights</p>
         <a href="https://github.com/raphaeloyelami">See My Work</a>
       </div>
