4fac65737fa7b51b561c84f1e0f3fb1e3dc7d949
Reyinbee <wealthbassey99@gmail.com>
2025-01-01 11:26:06 +0100

    main

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import './Contact.css';
-import phoneIcon from '../assets/phone-icon.png';
 import emailIcon from '../assets/email-icon.png';
-import linkendin from '../assets/linkendin.png';
+import linkedinIcon from '../assets/linkendin.png'; 
 
 const Contact = () => {
   const contactDetails = [
     {
-      text: 'Phone: +234 705 298 7755',
-      icon: phoneIcon,
-      link: 'tel:+2347052987755',
-    },
-    {
-      text: 'Email: raphaeloyelami1@gmail.com ',
+      // text: 'Email: raphaeloyelami1@gmail.com',
       icon: emailIcon,
-      link: 'mailto:raphaeloyelami1@gmail.com ',
+      link: 'mailto:raphaeloyelami1@gmail.com',
     },
     {
-      text: 'LinkedIn: https://www.linkedin.com/in/raphael-oyelami/',
-      icon: linkendin,
+      // text: 'LinkedIn: https://www.linkedin.com/in/raphael-oyelami/',
+      icon: linkedinIcon,
       link: 'https://www.linkedin.com/in/raphael-oyelami/',
     },
   ];
@@ -30,8 +24,10 @@ const Contact = () => {
       <div className="contact-info">
         {contactDetails.map((detail, index) => (
           <div key={index} className="contact-info-item">
-            <img src={detail.icon} alt="" className="contact-icon" />
-            <a href={detail.link}>{detail.text}</a>
+            <a href={detail.link} target="_blank" rel="noopener noreferrer">
+              <img src={detail.icon} alt={detail.text} className="contact-icon" />
+            </a>
+            <p>{detail.text}</p>
           </div>
         ))}
       </div>
